a6b1ebcd219e9c97dd4a08115b09b6543bcc7eaf
Erica Little <eteney90@gmail.com>
2019-11-07 01:08:19 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.scan(/[A_Z].*\W$/) ? true : false
+text.match(/[A_Z]\W$/) ? true : false
 end
 
 def valid_phone_number?(phone)
